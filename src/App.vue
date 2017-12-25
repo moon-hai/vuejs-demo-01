cb5a2a5b5faead88a7c26f7ac0cbb87fbec40344
moon-hai <moon.hai@1pac.vn>
2017-12-25 14:43:07 +0700

    remake route

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
     <app-header></app-header>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-            <router-view/>
-          </div>
-        </div>
-      </div>
+      <router-view/>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  import Footer from './components/Footer.vue'
+  import Header from './components/shared/Header.vue'
+  import Footer from './components/shared/Footer.vue'
 
   export default {
     name: 'app',
