9bf4096e21a1ce65692db3a009648daf605d7dc3
DiamondDat <diamond.dat@1pac.vn>
2018-01-03 16:53:21 +0700

    Fix some bugs

--- src/components/tag/Tag.vue
+++ src/components/tag/Tag.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="{name: 'tag', params: {name: tag}}" tag="li">
-    <a class="label label-pill label-default">{{ tag }}<span>  - -  </span></a></router-link>
+    <a class="label label-pill label-default">{{ tag }}</a>&nbsp;</router-link>
 </template>
 
 <script>
